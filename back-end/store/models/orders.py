c5100cd15ee4f88c25f83d4d5e92a77f70441800
Mohammad Razzaghi <mohammad01webdev@proton.me>
2025-12-23 14:24:21 -0500

    chore: Order Models

---
+++ back-end/store/models/orders.py
@@ -0,0 +1,22 @@
+from db.redis import redis
+from redis_om import HashModel
+
+
+class ProductOrder(HashModel):
+    product_id: str
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
